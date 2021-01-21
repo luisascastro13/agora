8d5e8e87f209b41c338056f8975f635e2dfd463f
Luísa de Castro <luisascastro13@gmail.com>
2021-01-21 17:09:02 -0300

    Espaço de edição de ata pronto

--- src/app/view/visualizarReuniao.php
+++ src/app/view/visualizarReuniao.php
@@ -40,59 +40,54 @@ $dataFormatada = date_format($data, 'd/m/Y à\s H:i');
 <html>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
+ <script type="text/javascript" src="js/js_1.9/jquery-1.8.2.js"></script>  
+    <script type="text/javascript" src="js/js_1.9/jquery-ui-1.9.1.custom.min.js"></script>  
+
+<head>
+</head>
+
+<body>
+
 <a href="visualizarNucleo.php?id=<?=$idNucleo?>">VOLTAR</a>
 
 <h1><?="Reunião: ".$reuniao->getNome();?></h1>
 <h2><?="Data e horário: ".$dataFormatada?></h2>
-<h3>
-	<?php
-	if($reuniao->getIdAta() != null){
-		echo "Documentos:";
-	}?>
-</h3>
 
 
-<div class="form-floating">
-  <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 50%; width: 70%;"></textarea>
-  <label for="floatingTextarea2">Comments</label>
-</div>
-<?php 
-if($userAdm==true){ ?>
+<!-- mostra espaço de edição da ata para o usuario adm -->
+<?php if($userAdm==true){ ?>
 	<a href="#" class="btn btn-sm btn-outline-warning">Editar Reunião</a>
-<?php } ?>
 
+   <?php if($reuniao->getIdAta() != null OR $reuniao->getIdListapresenca() != null OR $reuniao->getIdVotacao() != null){ 
+      echo "<h3>Documentos:</h3>"; ?>
 
-
-
-
-<!-- <div class="accordion" id="accordionExample">
- <div class="accordion-item">
+      <div class="accordion" id="accordionExample">
+  <div class="accordion-item">
     <h2 class="accordion-header" id="headingOne">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">Ata
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+        Ata
       </button>
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-      	<div class="form-check form-switch">
-		  <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
-		  <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
-		</div>
-		
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+
+        <?php if($reuniao->getIdAta() != null){?>
+          <iframe src="editorAta.html" title="Editor da Ata" width="100%"></iframe>       
+        
+      <?php } ?>
+
+        
       </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingTwo">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Lista de Presença 
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Accordion Item #2
       </button>
     </h2>
     <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-      	<div class="form-check form-switch">
-		  <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
-		  <label class="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
-		</div>
         <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
       </div>
     </div>
@@ -100,7 +95,7 @@ if($userAdm==true){ ?>
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingThree">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Votação
+        Accordion Item #3
       </button>
     </h2>
     <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
@@ -109,9 +104,24 @@ if($userAdm==true){ ?>
       </div>
     </div>
   </div>
-</div> -->
+</div>
+
+  
+      
+
+      <?php if($reuniao->getIdListaPresenca() != null){ 
+        echo "<h4>Lista presenca:</h4>"; ?>
+
+        aqui vai ter a lista ok<br>
+        
+        <a href="#" class="btn btn-sm btn-outline-info">Lista Presenca</a>
+      <?php } ?>
+  <?php } ?>
+<?php } ?>
+</body>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
 </html>
 
 
